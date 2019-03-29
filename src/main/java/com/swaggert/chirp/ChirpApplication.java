ff6ea0fe8ec66c400be0ef3abda578c32c9cdec6
Ryan Swaggert <rswagger@lakeheadu.ca>
2019-03-29 16:38:24 -0400

    Initial Spring files

---
+++ src/main/java/com/swaggert/chirp/ChirpApplication.java
@@ -0,0 +1,13 @@
+package com.swaggert.chirp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChirpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChirpApplication.class, args);
+	}
+
+}
