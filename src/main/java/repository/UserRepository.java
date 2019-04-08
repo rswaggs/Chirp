911265cdedd7f0a908ae781c81fd66a365b9a991
Ryan Swaggert <rswagger@lakeheadu.ca>
2019-04-07 21:51:23 -0400

    Model part 1-b
    Taking a break from this project.  At same place as the last commit.

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, String> {
 
+	// Can I define queries by these function calls?
+	User findByUsername(String username);
 }
